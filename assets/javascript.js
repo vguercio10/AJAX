ea1a18c916dd411fb8b7f10fe605b6f525c60eef
Jessica Jones <47366649+vguercio10@users.noreply.github.com>
2019-03-12 15:39:36 -0400

    pausing gifs added

--- assets/javascript.js
+++ assets/javascript.js
@@ -36,12 +36,17 @@ $(document).ready(function () {
                 
            // Storing array of results in results variable 
             var results = response.data;
-            console.log(response);
-
+            console.log(results);
+           
             for (var i = 0; i < results.length; i++) {
-                    
+                var still = results[i].images.fixed_height_still.url
+                var animate = results[i].images.fixed_height.url
                 var topicImage = $("<img>");
-                topicImage.attr("src", results[i].images.fixed_height.url);
+                topicImage.attr("src", still);
+                topicImage.attr("data-state", "still");
+                topicImage.attr("data-animate", animate);
+                topicImage.attr("data-still", still);
+                topicImage.addClass("pic")
                 // gifs.append(p);
                 $("#gifImages").append(topicImage);
                 // $("#gifImages").prepend(gifs);
@@ -53,13 +58,27 @@ $(document).ready(function () {
                 var rating = results[i].rating;
                 var p = $("<p>").text("Rating: " + rating);
                 $("#gifImages").append(p);
+            
             }
         }
             // When data comes back from API
     });          
             
 });
-    
+$(document).on("click", ".pic", function() {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
     
 
                     //loop through the results and display the images just like the buttons
